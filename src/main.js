67ed81c2487c5a7251ad449d5f6285f4e651beea
Jens Laufer <jens.laufer@solytics.de>
2025-11-20 09:47:45 +0100

    fixed head

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { createHead } from '@unhead/vue/client'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
@@ -6,4 +7,5 @@ import './style.css'
 
 createApp(App)
     .use(router)
+    .use(createHead())
     .mount('#app')
